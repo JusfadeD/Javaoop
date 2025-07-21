502066fc010cde025c616a07630d1238abc94e89
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-21 15:06:58 +0400

    Completed Task 1 - Class and Object Basics

---
+++ Task1OOP/src/Task1.java
@@ -0,0 +1,18 @@
+public class Task1 {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.name = "Alice";
+        p.age = 25;
+        p.display();
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
