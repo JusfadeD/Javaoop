6c2e8ac9e57514ee8bcbb814e64db8dc9f6565fb
Abdullah Al Rashdi <22-0673@student.gutech.edu.om>
2025-07-21 15:11:20 +0400

    Completed Task 2 - Method in Class

--- Task1OOP/src/Task2.java
+++ Task1OOP/src/Task2.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Task2 {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        rect.length = 10.0;
+        rect.width = 5.0;
+        System.out.println("Area of Rectangle: " + rect.calculateArea());
+    }
+}
+
+class Rectangle {
+    double length;
+    double width;
+
+    // Method to calculate area
+    double calculateArea() {
+        return length * width;
+    }
 }
